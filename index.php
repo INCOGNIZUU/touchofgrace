6670642c38e932203fe8e531124e5fb89fa6b99c
Raymond Fajiculay <raymondfajiculay@Raymonds-MacBook.local>
2022-03-21 18:57:40 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/index.css">
+    <title>Log In</title>
+</head>
+
+<body>
+    <main>
+        <div class="container">
+            <img class="logo" src="images/logo-whole.png" alt="">
+            <h1>Touch of Grace</h1>
+            <form class="login" action="submit">
+                <input type="text" placeholder="Username">
+                <input type="password" placeholder="Password">
+                <button>Login</button>
+            </form>
+        </div>
+    </main>
+</body>
+
+</html>
